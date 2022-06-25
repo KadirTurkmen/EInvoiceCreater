a9b02c68eb569b04f00b1e6c7352d2f40e1096d9
Kadir <kturkmen@insoftpro.com>
2022-06-25 12:11:32 +0300

    Project created.

---
+++ Inowex.EInvoiceCreater/Inowex.EInvoiceCreater/EInvoiceCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inowex.EInvoiceCreater
+{
+    public interface IEInvoiceCreate
+    {
+    }
+
+    public class EInvoiceCreate : IEInvoiceCreate
+    {
+
+    }
+}
